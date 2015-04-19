d653c552a86d93d56ddc3b214db1b16ce8aea5d9
Ulrich Diedrichsen <ulrich.diedrichsen@googlemail.com>
2015-04-19 09:18:17 +0200

    First version

---
+++ package.js
@@ -0,0 +1,50 @@
+Package.describe({
+  name: 'doedel:reveal-js',
+  summary: 'Reveal.js - HTML Presentation Framework',
+  version: '3.0.2',
+  git: 'https://github.com/udiedrichsen/meteor-reveal-js.git'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom("1.0.1");
+  api.use(["meteor"]);
+
+  api.addFiles([
+	'reveal.js/css/theme/beige.css',
+	'reveal.js/css/theme/black.css',
+	'reveal.js/css/theme/blood.css',
+	'reveal.js/css/theme/league.css',
+	'reveal.js/css/theme/moon.css',
+	'reveal.js/css/theme/night.css',
+	'reveal.js/css/theme/serif.css',
+	'reveal.js/css/theme/simple.css',
+	'reveal.js/css/theme/sky.css',
+	'reveal.js/css/theme/solarized.css',
+	'reveal.js/css/theme/white.css',
+	'reveal.js/lib/css/zenburn.css',
+	'reveal.js/lib/font/league-gothic/league-gothic.css',
+	'reveal.js/lib/font/league-gothic/league-gothic.eot',
+	'reveal.js/lib/font/league-gothic/league-gothic.ttf',
+	'reveal.js/lib/font/league-gothic/league-gothic.woff',
+    'reveal.js/lib/font/source-sans-pro/source-sans-pro-italic.eot',
+    'reveal.js/lib/font/source-sans-pro/source-sans-pro-italic.ttf',
+    'reveal.js/lib/font/source-sans-pro/source-sans-pro-italic.woff',
+    'reveal.js/lib/font/source-sans-pro/source-sans-pro-regular.eot',
+    'reveal.js/lib/font/source-sans-pro/source-sans-pro-regular.ttf',
+    'reveal.js/lib/font/source-sans-pro/source-sans-pro-regular.woff',
+    'reveal.js/lib/font/source-sans-pro/source-sans-pro-semibold.eot',
+    'reveal.js/lib/font/source-sans-pro/source-sans-pro-semibold.ttf',
+    'reveal.js/lib/font/source-sans-pro/source-sans-pro-semibold.woff',
+    'reveal.js/lib/font/source-sans-pro/source-sans-pro-semibolditalic.eot',
+    'reveal.js/lib/font/source-sans-pro/source-sans-pro-semibolditalic.ttf',
+    'reveal.js/lib/font/source-sans-pro/source-sans-pro-semibolditalic.woff',
+    'reveal.js/lib/font/source-sans-pro/source-sans-pro.css'
+  ], 'client', { isAsset: true });
+
+  api.addFiles([
+	'reveal.js/js/reveal.js',
+	'reveal.js/css/reveal.css'
+  ], 'client');
+
+  api.export('Reveal', 'client');
+});
